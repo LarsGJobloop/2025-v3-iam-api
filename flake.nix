6724b4681f862fdcf2b86189f68febb7cb658778
zabronax <zabronax@gmail.com>
2025-06-12 10:07:20 +0200

    setup project environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ".NET API integrated with GitHub OAuth";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      withPkgs = callback: withSystem (system: callback (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            dotnetCorePackages.sdk_9_0 # Program Development
+          ];
+
+          env = {
+            DOTNET_ROOT = builtins.toString pkgs.dotnetCorePackages.sdk_9_0;
+          };
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}

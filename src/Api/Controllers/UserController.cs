d8e8b004cf5e42cdf07041fbae9d9f310688decb
zabronax <zabronax@gmail.com>
2025-06-12 10:23:37 +0200

    remove example code

---
+++ src/Api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetUser")]
+    public bool Get()
+    {
+        return true;
+    }
+}
